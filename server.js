327c3571e8dc335c025c3f9ed194682c5e3d555a
marine <marine@MarinedeiMac.local>
2020-06-17 22:17:04 +0800

    register user to login table with transaction

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ const db = knex({
 });
 
 // test
-// since this return a promise, do .then to access data.
-// no need to do .json since it doesn't pass through HTTP
 // db.select("*").from('users').then(data => console.log(data)); // []
 
 const app = express();
@@ -65,11 +63,10 @@ app.get('/', (req, res) => {
 
 
 // user signin with data in body
+
 app.post('/signin', (req, res) => {
     const { email, password } = req.body
-    // bcrypt.compare("gogogo", "$2a$10$a3OAAVGjKNDxpiawYKx6k.OszB.vnXhkeNluPFJRYZxB2ozzAZVZm", (err, res) => {
-    //     console.log(res);
-    // });
+
     if( email === database.users[0].email && 
         password === database.users[0].password){
             res.json(database.users[0]);
@@ -82,26 +79,51 @@ app.post('/signin', (req, res) => {
 
 
 // new user sign up with data in body
+// add data to 2 tables: login and users,
+// store hash in login
+
+// also, use transaction to make sure that the data will add to 2 tables at one time
+// 使用者輸入：姓名、mail、密碼註冊，要把資料存到login以及users
 app.post('/register', (req, res) => {
-    const { name, email, password} = req.body;
-    db('users')
-    .returning("*") // return all columns of that user
-    // specifies which column should be returned by insert. update . 
-    .insert({
-        name: name,
-        email: email,
-        joined: new Date()
+    const { name, email, password } = req.body;
+    // encrypted password
+    const hash = bcrypt.hashSync(password);
+    // transaction begin
+    db.transaction(trx => {
+        return trx.insert({
+            email: email,
+            hash: hash
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx
+            .returning('*')
+            .insert({
+                email: loginEmail[0],  // {"...@gmail.com"}
+                name: name,
+                joined: new Date()
+            })
+            .into('users')           
+            .then(user => res.json(user[0]))
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
-    .then(user => res.json(user[0])) // w/o [0], it returns an array
-    // encrypt password 
-    // bcrypt.hash(password, null, null, (err, hash) => {
-    //     hashedPw = hash;
-    // });
-    .catch(err => res.status(400).json("fail to register!!"));
+    .catch(error => console.log(error))
+
+    // db('users')
+    // .insert({
+    //     name: name,
+    //     email: email,
+    //     joined: new Date()
+    // })
+    // .returning("*") 
+    // .then(user => res.json(user[0])) // w/o [0], it returns an array
+    // .catch(err => res.status(400).json("fail to register!!"));
 })
 
 // get user profile through id
-// 後來不寫/:id了，findUserFun有改，改成body送資料
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
     db.select('*').from('users').where({
@@ -117,30 +139,15 @@ app.get('/profile/:id', (req, res) => {
     .catch(error => res.status(400).json('cannot find user'));
 })
 
-// 帶入/:id 及 flag，找到userIndex
-const findUserFun = (user, finded) => {
-    const { id } = user;
-    const numId = parseInt(id);
-    let userIndex = 0;
-    const findUser = database.users.some((item, index) => {
-        if( item.id === numId){
-            finded = true;
-            userIndex = index;
-            return item.id === numId;
-        }
-    })
-    return [userIndex, finded];
-}
 
 // every time when user send an url, increase the entries.
 app.put('/image', (req, res) => {
     const { id } = req.body;
     db('users')
-   .where({id: id})
-   .returning('entries')
-   .increment('entries', 1)
+    .where({id: id})
+    .returning('entries')
+    .increment('entries', 1)
     .then(data => res.json(data));
-
 })
 
 
