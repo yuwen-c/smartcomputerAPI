30e10813da1a1faf35d67a7a37ee37ffd5c9febe
marine <marine@MarinedeiMac.local>
2020-07-20 16:34:59 +0800

    fix issue of normal registration, which results error of cannot find id of empty array

--- controllers/register.js
+++ controllers/register.js
@@ -4,39 +4,39 @@ const handleRegister = (req, res, db, bcrypt) => {
         // check if the email has already existed
         db('users').where('email', email)
         .then(data => {
-            if(data[0].id){ // if the email exists, don't do registration
-                res.status(400).json("this email address has been registered!");
-                console.log('this email address has been registered!')
-            }
-            else{
+            if(data.length === 0){  // email is new, data is [ ]
                 // encrypted password
                 const hash = bcrypt.hashSync(password);
                 // transaction begin
                 db.transaction(trx => {
-                    trx.insert({
-                        email: email,
-                        hash: hash
-                    })
-                    .into('login')
-                    .returning('email')
-                    .then(loginEmail => {
-                        return trx
-                        .returning('*')
-                        .insert({
-                            email: loginEmail[0],  // {"...@gmail.com"}
-                            name: name,
-                            joined: new Date()
-                        })
-                        .into('users')           
-                        .then(user => res.json(user[0]))
-                    })
-                    .then(trx.commit)
-                    .catch(trx.rollback)
+                                    trx.insert({
+                                        email: email,
+                                        hash: hash
+                                    })
+                                    .into('login')
+                                    .returning('email')
+                                    .then(loginEmail => {
+                                        return trx
+                                        .returning('*')
+                                        .insert({
+                                            email: loginEmail[0],  // {"...@gmail.com"}
+                                            name: name,
+                                            joined: new Date()
+                                        })
+                                        .into('users')           
+                                        .then(user => res.json(user[0]))
+                                    })
+                                    .then(trx.commit)
+                                    .catch(trx.rollback)
                 })
                 .catch(error => {
                     res.status(400).json("registration failed")
                     console.log("registration failed");
                 })
+            }
+            else if(data[0].id){ // mail exists, don't do registration
+                res.status(400).json("this email address has been registered!");
+                console.log('this email address has been registered!')
             }
         });
     }
