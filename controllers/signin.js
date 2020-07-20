50583742aeac5b97ab6f1e7f65f631f959a071bd
marine <marine@MarinedeiMac.local>
2020-07-20 17:28:22 +0800

    check empty blank in signin

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,24 +1,29 @@
 const handleSignin = (req, res, db, bcrypt) => {
     const { email, password } = req.body
-    db.select('hash').from('login')
-    .where({email: email}) 
-    .then(data=> {
-        // if the email exists
-        if(data.length){
-            const isValid = bcrypt.compareSync(password, data[0].hash); 
-            if(isValid){
-                return db.select('*').from('users')
-                .where({email: email})
-                .then(user => res.json(user[0])) 
-            }else{
-                res.status(400).json('wrong credentials!') // wrong password
+    if(email && password){
+        db.select('hash').from('login')
+        .where({email: email}) 
+        .then(data=> {
+            // if the email exists, do find a user(data)
+            if(data.length){
+                const isValid = bcrypt.compareSync(password, data[0].hash); // compare if password is correct
+                if(isValid){
+                    return db.select('*').from('users')
+                    .where({email: email})
+                    .then(user => res.json(user[0])) 
+                }else{
+                    res.status(400).json('wrong credentials') // wrong password
+                }
             }
-        }
-        else{
-            res.status(400).json('wrong credentials') // email doesn't exist
-        }
-    }) 
-    .catch(err => res.status(400).json("fail to sing in"))   
+            else{
+                res.status(400).json('wrong credentials') // email doesn't exist
+            }
+        }) 
+        .catch(err => res.status(400).json("fail to sing in"))   
+    }
+    else{
+        res.status(400).json('please fill in the blanks')
+    }
 }
 
 module.exports = {
