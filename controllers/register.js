2094c6f92404fa3ff4a586914964ac7cd82c19c9
marine <marine@MarinedeiMac.local>
2020-07-20 15:54:10 +0800

    add 3 failure conditions in register, empty blank, duplicate, registration failes

--- controllers/register.js
+++ controllers/register.js
@@ -1,30 +1,49 @@
 const handleRegister = (req, res, db, bcrypt) => {
     const { name, email, password } = req.body;
-    // encrypted password
-    const hash = bcrypt.hashSync(password);
-    // transaction begin
-    db.transaction(trx => {
-        trx.insert({
-            email: email,
-            hash: hash
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx
-            .returning('*')
-            .insert({
-                email: loginEmail[0],  // {"...@gmail.com"}
-                name: name,
-                joined: new Date()
-            })
-            .into('users')           
-            .then(user => res.json(user[0]))
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(error => console.log(error))
+    if(name && email && password){  // check no blank 
+        // check if the email has already existed
+        db('users').where('email', email)
+        .then(data => {
+            if(data[0].id){ // if the email exists, don't do registration
+                res.status(400).json("this email address has been registered!");
+                console.log('this email address has been registered!')
+            }
+            else{
+                // encrypted password
+                const hash = bcrypt.hashSync(password);
+                // transaction begin
+                db.transaction(trx => {
+                    trx.insert({
+                        email: email,
+                        hash: hash
+                    })
+                    .into('login')
+                    .returning('email')
+                    .then(loginEmail => {
+                        return trx
+                        .returning('*')
+                        .insert({
+                            email: loginEmail[0],  // {"...@gmail.com"}
+                            name: name,
+                            joined: new Date()
+                        })
+                        .into('users')           
+                        .then(user => res.json(user[0]))
+                    })
+                    .then(trx.commit)
+                    .catch(trx.rollback)
+                })
+                .catch(error => {
+                    res.status(400).json("registration failed")
+                    console.log("registration failed");
+                })
+            }
+        });
+    }
+    else{
+        res.status(400).json("please fill in the blanks");
+        console.log("please fill in the blanks")
+    }
 }
 
 module.exports = {
