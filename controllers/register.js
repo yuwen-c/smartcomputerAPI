9b0900eb8f27d18dc9e90d2ed9e5e1b1687587dd
marine <marine@MarinedeiMac.local>
2020-07-23 17:49:38 +0800

    delete unnecessary console

--- controllers/register.js
+++ controllers/register.js
@@ -9,40 +9,37 @@ const handleRegister = (req, res, db, bcrypt) => {
                 const hash = bcrypt.hashSync(password);
                 // transaction begin
                 db.transaction(trx => {
-                                    trx.insert({
-                                        email: email,
-                                        hash: hash
-                                    })
-                                    .into('login')
-                                    .returning('email')
-                                    .then(loginEmail => {
-                                        return trx
-                                        .returning('*')
-                                        .insert({
-                                            email: loginEmail[0],  // {"...@gmail.com"}
-                                            name: name,
-                                            joined: new Date()
-                                        })
-                                        .into('users')           
-                                        .then(user => res.json(user[0]))
-                                    })
-                                    .then(trx.commit)
-                                    .catch(trx.rollback)
+                    trx.insert({
+                        email: email,
+                        hash: hash
+                    })
+                    .into('login')
+                    .returning('email')
+                    .then(loginEmail => {
+                        return trx
+                        .returning('*')
+                        .insert({
+                            email: loginEmail[0],  // {"...@gmail.com"}
+                            name: name,
+                            joined: new Date()
+                        })
+                        .into('users')           
+                        .then(user => res.json(user[0]))
+                    })
+                    .then(trx.commit)
+                    .catch(trx.rollback)
                 })
                 .catch(error => {
                     res.status(400).json("registration failed")
-                    console.log("registration failed");
                 })
             }
             else if(data[0].id){ // mail exists, don't do registration
                 res.status(400).json("this email address has been registered!");
-                console.log('this email address has been registered!')
             }
         });
     }
     else{
         res.status(400).json("please fill in the blanks");
-        console.log("please fill in the blanks")
     }
 }
 
