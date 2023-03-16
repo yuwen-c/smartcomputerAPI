09b1276df2ef16ee3d46b407ab88cc561709d88d
yuwen <yuwen.azulejos@gmail.com>
2023-03-16 15:35:33 +0800

    chore:change config of knex connection between local server to database on heroku

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 module.exports = {
-    endpoint: process.env.CLARIFAI_API_URL,
-    apiKey: process.env.CLARIFAI_API_KEY,
-    port: process.env.PORT,
-    database: process.env.DATABASE_URL
+  endpoint: process.env.CLARIFAI_API_URL,
+  apiKey: process.env.CLARIFAI_API_KEY,
+  port: process.env.PORT,
+  database: process.env.DATABASE_URL,
+
+  KNEX_HOST: process.env.KNEX_HOST,
+  KNEX_USER: process.env.KNEX_USER,
+  KNEX_PASSWORD: process.env.KNEX_PASSWORD,
+  KNEX_DATABASE: process.env.KNEX_DATABASE,
 };
--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-const { port, database } = require('./config');
+const {
+  port,
+  database,
+  KNEX_HOST,
+  KNEX_USER,
+  KNEX_PASSWORD,
+  KNEX_DATABASE,
+} = require('./config');
 
 // use knex to connect database to server
 // const db = knex({
@@ -19,55 +26,68 @@ const { port, database } = require('./config');
 //     }
 // });
 
-// connecting postgre on heroku 
-const db = knex({
-    client: 'pg',  // postgre
-    connection: {
-        connectionString: database,
-        ssl: {
-          rejectUnauthorized: false
-        }
-    }
-});
+// connecting postgre on heroku
+// const db = knex({
+//     client: 'pg',  // postgre
+//     connection: {
+//         connectionString: database,
+//         ssl: {
+//           rejectUnauthorized: false
+//         }
+//     }
+// });
 
+// connect local backend server to database on heroku
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: KNEX_HOST,
+    user: KNEX_USER,
+    password: KNEX_PASSWORD,
+    database: KNEX_DATABASE,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const app = express();
 
 // middlewear to decode req.body
 app.use(cors());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // test if server works
 app.get('/', (req, res) => {
-    res.json('connecting to server');
-    // console.log('console: connecting!!');
-})
+  res.json('connecting to server');
+  // console.log('console: connecting!!');
+});
 
 // user signin with data in body
-app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt))  
+app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
 
 // new user sign up with data in body
 // add data to 2 tables: login and users,
 // store hash in login
 // also, use transaction to make sure that the data will add to 2 tables at one time
-app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt))
+app.post('/register', (req, res) =>
+  register.handleRegister(req, res, db, bcrypt)
+);
 
 // get user profile through id
-app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db))
+app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
 
 // send url from front to back end, and make clarifai api request to get face-recog data
-app.post('/imageUrl', image.handleApiCall)
+app.post('/imageUrl', image.handleApiCall);
 
 // every time when user send an url, increase the entries.
-app.put('/image', image.handleImage(db))
-
+app.put('/image', image.handleImage(db));
 
 // app.listen(process.env.PORT || 3000, ()=> {
 //     console.log(`it's running on port ${process.env.PORT}!`);
 // })
 
 app.listen(port || 3000, () => {
-    console.log(`it's running on port ${port} based on the dotenv!`)
-})
-
+  console.log(`it's running on port ${port} based on the dotenv!`);
+});
